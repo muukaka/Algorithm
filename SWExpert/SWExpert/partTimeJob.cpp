3c65bb2aced5a27dae6b0771319ac06ffcfe93e9
Mukaka <hsw403hsw@naver.com>
2018-04-10 15:19:30 +0900

    LTest Problem solving - PartTimeJob

--- SWExpert/SWExpert/partTimeJob.cpp
+++ SWExpert/SWExpert/partTimeJob.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>// max()
+#include <stack>
 
 using namespace std;
 
@@ -18,10 +19,11 @@ int M;// 알바 시작날, 0 <= M <= 200
 int D;// 알바 기간, 0 <= D <= 100
 int I;// 보수, 0 <= I <= 100
 int s[501][3];//알바 스케줄, M D I
-int chck[501] = { 0, };// 선택한 알바는 1을 표시, 출력을 위해 사용
+//int chck[501] = { 0, };// 선택한 알바는 1을 표시, 출력을 위해 사용
 int day[301] = { 0, };// 알바하는 날짜, 0 ~ 300
-int maxi;
 int ret;
+vector<int> alist;
+
 
 // 점화식 : alba(n) = max{ alba(n + 1), alba(n + 1) + s[n][2] }
 // 복잡도 O(2^n) <- 10개정도까지밖에 계산못함
@@ -41,27 +43,51 @@ int alba(int n)
 	else
 	{
 		sum = alba(n + 1);
+
 		for (int i = s[n][0]; i < s[n][0] + s[n][1]; i++)
 			day[i] = 1;
 		sum = max(sum, alba(n + 1) + s[n][2]);
+
 		for (int i = s[n][0]; i < s[n][0] + s[n][1]; i++)
 			day[i] = 0;
 	}
 	return sum;
 }
-//int* dp(int* chck, int num)
-//{
-//	// base : 알바번호 > N 인 경우 (끝까지 다 살펴봄)
-//	if (num > N)
-//		return;
-//	// base : 알바 기간이 겹치는 경우, 다음 알바번호를 고려
-//
-//
-//
-//	return chck;
-//	// 수입이 최대일 경우에, vector<알바>를 리턴하는건 어떨까?
-//	// 최근에 이렇게 푸는거 어디서 봤더라...
-//}
+
+// 처음부터 알바의 합이 최대값과 같을 때 그 때의 알바 리스트를 출력
+// n : 현재 알바 번호, sum : 현재까지 보수의 합, m : 보수의 최대합
+void albaList(int n, int sum, int m)
+{
+	if (n > N)
+		return;
+	if (sum == m)
+	{
+		for (int i = 0; i < alist.size(); i++)
+			cout << s[alist[i]][0] << " " << s[alist[i]][1] << endl;
+		return;
+	}
+
+	int check = 0;
+	for (int i = s[n][0]; i < s[n][0] + s[n][1]; i++)
+		if (day[i] == 1)
+			check = 1;
+
+	if (check == 1)
+		albaList(n + 1, sum, m);
+	else
+	{
+		for (int i = s[n][0]; i < s[n][0] + s[n][1]; i++)
+			day[i] = 1;
+		alist.push_back(n);
+		albaList(n + 1, sum + s[n][2], m);
+
+		for (int i = s[n][0]; i < s[n][0] + s[n][1]; i++)
+			day[i] = 0;
+		alist.pop_back();
+		albaList(n + 1, sum, m);
+	}
+	return;
+}
 
 int main()
 {
@@ -71,24 +97,14 @@ int main()
 		cin >> s[i][0] >> s[i][1] >> s[i][2];// M D I
 
 	// algor
-	// 두가지 함수로 나눠야 할텐데...
-	// 최대보수를 찾는 함수와, 그때의 알바 스케줄을 구하는 함수?
-	// 전달해줘야 할 정보는? 알바하는 기간, 현재까지 보수의 총합(?), 현재 알바번호
-	// 알바를 선택했는지, 보수의합이 얼만지
-
-	//dp(chck, 0);// 알바 0번부터 고려
-
-	// alba(n, sum) = 현재 알바 번호가 n이고, 지금까지의 보수의 합이 sum일때
-	// 알바 리스트를 맨뒤까지 고려하여 얻을 수 있는 최대 보수 합을 반환한다.
+	// alba(n) = 현재 알바 번호가 n이고, 현재로부터 얻을 수 있는 보수의 최대합을 반환
+	// 알바 리스트를 맨뒤까지 고려하여 얻을 수 있는 보수의 최대합을 반환
 	ret = alba(0);
-	// 점화식 : alba(n, sum) = max{ alba(n+1, sum), alba(n+1, sum+s[n][2]) }
-	// 메모이제이션 : n(3~500), sum(0~50000) <- 근데 이거 알바를 순차적으로 도는건데 메모이제이션을 할수가 없는데
-
 
 	// output
 	cout << endl;
 	cout << ret << endl;
-	cout << "알바 목록" << endl;
-
+	//cout << "알바 목록" << endl;
+	albaList(0, 0, ret);
 	return 0;
 }
